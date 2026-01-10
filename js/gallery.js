9dbe996a983f1a1e315ec7d37270fa74ecc04ab2
vortexvem-lgtm <vortexvem@gmail.com>
2026-01-10 14:21:50 -0600

    Add files via upload

--- js/gallery.js
+++ js/gallery.js
@@ -1,1 +1,13 @@
+const gallery = document.getElementById('gallery');
 
+// How many images are in your folder
+const totalImages = 4; // <-- update this if you add more
+
+for (let i = 1; i <= totalImages; i++) {
+  const img = document.createElement('img');
+  img.src = `images/gallery/${i}.jpg`; // 1.jpg, 2.jpg, etc.
+  img.alt = `Artwork ${i}`;
+  img.className = "gallery-item";
+
+  gallery.appendChild(img);
+}
